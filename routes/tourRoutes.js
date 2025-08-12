beb951294a9e18ce01b8518d9b588b848dab5d8a
AbyalewLobe <labyalew@gmail.com>
2025-08-12 01:45:53 -0700

    ..

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,6 +6,6 @@ import { getAllTours } from '../controllers/tourController.js'
 const router = express.Router()
 
 router.get('/tour', test)
-router.get('/tours', getAllTours)
+router.get('/', getAllTours)
 
 export default router
