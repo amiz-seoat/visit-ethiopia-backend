1e11ecb17eab8adfc4bbb5907e708f1bded8e0cb
AbyalewLobe <labyalew@gmail.com>
2025-08-12 22:07:01 -0700

    ..

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -11,7 +11,7 @@ const router = express.Router()
 
 router.get('/tour', test)
 router.get('/', getAllTours)
-router.get('/:id', getTour)
 router.get('/featured', featuredTours, getAllTours)
+router.get('/:id', getTour)
 
 export default router
