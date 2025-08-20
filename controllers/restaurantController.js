efc3d76559797185541538111194b438a705db7d
muslim25351 <muslim25351@gmail.com>
2025-08-19 22:37:45 -0700

    add get all restaurant, get restaurant by id, get featured restaurants,update and delete restaurant

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,6 +1,7 @@
 import Restaurant from '../models/Restaurants.js'
 import catchAsync from '../utils/catchAsync.js'
 import AppError from '../utils/appError.js'
+import factory from './handlerFactory.js'
 
 export const test = catchAsync(async (req, res) => {
   res.status(201).json({
@@ -8,3 +9,17 @@ export const test = catchAsync(async (req, res) => {
     message: 'test file',
   })
 })
+
+//filter for featured restaurants
+export const featuredRestaurants = (req, res, next) => {
+  req.query.isFeatured = 'true'
+  req.query.sort = '-averageRating,price'
+  next()
+}
+
+export const getAllRestaurants = factory.getAll(Restaurant)
+export const getRestaurant = factory.getOne(Restaurant, {
+  path: 'reviews createdBy',
+})
+export const updateRestaurant = factory.updateOne(Restaurant)
+export const deleteRestaurant = factory.deleteOne(Restaurant)
--- routes/restaurantrRoutes.js
+++ routes/restaurantrRoutes.js
@@ -1,9 +1,23 @@
 import express from 'express'
+import { protect, restrict } from '../controllers/authController.js'
 
-import { test } from '../controllers/restaurantController.js'
+import {
+  test,
+  getAllRestaurants,
+  getRestaurant,
+  featuredRestaurants,
+  updateRestaurant,
+  deleteRestaurant,
+} from '../controllers/restaurantController.js'
 
 const router = express.Router()
 
 router.get('/restaurant', test)
+
+router.get('/', getAllRestaurants)
+router.get('/featured', featuredRestaurants, getAllRestaurants)
+router.get('/:id', getRestaurant)
+router.patch('/:id', protect, restrict('admin'), updateRestaurant)
+router.delete('/:id', protect, restrict('admin'), deleteRestaurant)
 
 export default router
