49809bd24555415132f628558ec17334e96b7781
AbyalewLobe <labyalew@gmail.com>
2025-08-21 22:05:27 -0700

    add get all transports, get a single transport by id, and get featured transports

--- controllers/transportController.js
+++ controllers/transportController.js
@@ -1,6 +1,7 @@
 import Transport from '../models/Transport.js'
 import catchAsync from '../utils/catchAsync.js'
 import AppError from '../utils/appError.js'
+import factory from './handlerFactory.js'
 
 export const test = catchAsync(async (req, res) => {
   res.status(201).json({
@@ -8,3 +9,12 @@ export const test = catchAsync(async (req, res) => {
     message: 'test file',
   })
 })
+
+export const featuredTransports = (req, res, next) => {
+  req.query.isFeatured = 'true'
+  req.query.sort = '-averageRating,routes.price'
+  next()
+}
+
+export const getAllTransports = factory.getAll(Transport)
+export const getTransport = factory.getOne(Transport)
--- routes/transportRoutes.js
+++ routes/transportRoutes.js
@@ -1,9 +1,17 @@
 import express from 'express'
 
-import { test } from '../controllers/transportController.js'
+import {
+  test,
+  getAllTransports,
+  getTransport,
+  featuredTransports,
+} from '../controllers/transportController.js'
 
 const router = express.Router()
 
 router.get('/transport', test)
+router.get('/', getAllTransports)
+router.get('/featured', featuredTransports, getAllTransports)
+router.get('/:id', getTransport)
 
 export default router
