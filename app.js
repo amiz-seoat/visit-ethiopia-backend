1646651b317700772f6d2b2e27b14748ffd6f91b
AbyalewLobe <labyalew@gmail.com>
2025-08-30 02:52:22 -0700

    ..

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:5173',
+      'http://localhost:5175',
       'https://visit-ethiopia-frontend.vercel.app',
     ],
     credentials: true,
