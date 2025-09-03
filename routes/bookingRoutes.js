c440dbe5e4d76a0b40eb9b3f7e854b7f30a4b7af
AbyalewLobe <labyalew@gmail.com>
2025-09-02 23:10:14 -0700

    ....

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -20,6 +20,6 @@ router.get('/me', protect, getMyBookings)
 // Cancel a booking
 router.patch('/:id/cancel', protect, cancelBooking)
 // Update a booking
-router.patch('/:id', protect, restrict('admin'), updateBookingStatus)
+router.patch('/:id/status', protect, restrict('admin'), updateBookingStatus)
 
 export default router
