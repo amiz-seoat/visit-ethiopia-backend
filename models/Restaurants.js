1bf131d1a40d942bba455264926c7738aa14043c
AbyalewLobe <labyalew@gmail.com>
2025-09-05 01:25:51 -0700

    ......

--- models/Restaurants.js
+++ models/Restaurants.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose'
 
 const RestaurantSchema = new mongoose.Schema(
   {
--- models/Transport.js
+++ models/Transport.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose'
 
 const TransportSchema = new mongoose.Schema(
   {
