a42c13b2f9be8a7f251bec16ec68070e764ada3b
AbyalewLobe2 <abyalew120@gmail.com>
2025-09-15 03:09:55 -0700

    fix get current user booking

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -20,11 +20,12 @@ router.get('/', protect, restrict('admin'), getAllBookings)
 // Create new booking
 router.post('/', protect, createBooking)
 
+// Get current user's bookings
+router.get('/me', protect, getMyBookings)
+
 // Get a booking by id (owner or admin)
 router.get('/:id', protect, getBooking)
 
-// Get current user's bookings
-router.get('/me', protect, getMyBookings)
 // Cancel a booking
 router.patch('/:id/cancel', protect, cancelBooking)
 // Update a booking
